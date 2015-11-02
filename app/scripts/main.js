c6306003d856eee00f54f686f566d14ebe35eedf
Joshua Briley <josh@thebrileys.com>
2015-11-01 22:19:04 -0500

    Begin connection with Endpoint.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+function getRequest() {
+  var params = {
+    part: "Drum stick twirling",
+    key: 'AIzaSyCsJxPieXjWm2p5Ba8M187SjbGfq47OCTw',
+  };
+  endPoint = 'https://www.googleapis.com/youtube/v3/search/';
+  $.getJSON(endPoint, params, function(data){
+    //showResults();
+    console.log(data);
+  })
+}
+
+getRequest();
+
+
+
