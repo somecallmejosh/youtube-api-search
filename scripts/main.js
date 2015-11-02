26379079819b8c163f7bec3a70cc22310efa6d31
Joshua Briley <josh@thebrileys.com>
2015-11-01 22:39:58 -0500

    Reform api function.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-console.log("'Allo 'Allo!");
+function getRequest(){var e={part:"Drum stick twirling",key:"AIzaSyCsJxPieXjWm2p5Ba8M187SjbGfq47OCTw"};endPoint="https://www.googleapis.com/youtube/v3/search/",$.getJSON(endPoint,e,function(e){console.log(e)})}getRequest();
