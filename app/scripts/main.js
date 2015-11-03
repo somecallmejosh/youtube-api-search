e2f6cdfe215d13c5536fd15e162f92bae5e38825
Joshua Briley <josh@thebrileys.com>
2015-11-02 21:00:16 -0500

    Final touches

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,45 @@
-function getRequest() {
+function showResults(results) {
+  $.each(results, function(index, value){
+    var vidTitle = value.snippet.title,
+        vidId = value.id.videoId,
+        vidDescription = value.snippet.description,
+        vidImage = value.snippet.thumbnails.medium.url;
+    appendResults(vidTitle, vidId, vidDescription, vidImage ); 
+  })
+};
+
+function appendResults(title, ID, description, image) {
+  $('<div class="media"><div class="media-left"><a href="https://www.youtube.com/watch?v=' + ID + '" target="_blank"><img class="media-object" src="' + image + '" alt=""></a></div><div class="media-body"><h4 class="media-heading"> ' + title + '</h4><p>' + description + '</p><a href="https://www.youtube.com/watch?v=' + ID + '" class="btn btn-default" target="_blank">Watch Video</a></div></div>').appendTo(".well");
+};
+
+
+function getRequest(searchTerm) {
   var params = {
     part: "snippet",
     key: 'AIzaSyCsJxPieXjWm2p5Ba8M187SjbGfq47OCTw',
-    q: 'Drum Stick Twirling'
+    q: searchTerm
   };
   endPoint = 'https://www.googleapis.com/youtube/v3/search/';
   $.getJSON(endPoint, params, function(data){
-    console.log(data);
+    showResults(data.items);
+    console.log(data.items);
   });
-}
+};
 
-getRequest();
+$(function(){
+  $('form').submit(function(event){
+    event.preventDefault();
+    var getUserInput = $('.video-search').val();
+    getRequest(getUserInput);
+  })
+});
+
+function clearResults() {
+  $(".well .media").fadeOut();
+  $('.video-search').val("").focus();
+};
+
+$('.search-clear').on("click", clearResults);
 
 
 
