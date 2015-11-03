90206450e408485f560f71721c037f32b2300d32
Joshua Briley <josh@thebrileys.com>
2015-11-02 19:47:26 -0500

    Update endpoint info.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-function getRequest(){var e={part:"Drum stick twirling",key:"AIzaSyCsJxPieXjWm2p5Ba8M187SjbGfq47OCTw"};endPoint="https://www.googleapis.com/youtube/v3/search/",$.getJSON(endPoint,e,function(e){console.log(e)})}getRequest();
+function getRequest(){var e={part:"snippet",key:"AIzaSyCsJxPieXjWm2p5Ba8M187SjbGfq47OCTw",q:"Drum Stick Twirling"};endPoint="https://www.googleapis.com/youtube/v3/search/",$.getJSON(endPoint,e,function(e){console.log(e)})}getRequest();
